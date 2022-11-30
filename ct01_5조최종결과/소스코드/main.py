227049869f5a47de392d7a3ec0eb3b7f07965f7b
Majang (이정진) <65146431+Majang9711@users.noreply.github.com>
2022-11-30 22:34:09 +0900

    Add files via upload

---
+++ ct01_5조최종결과/소스코드/main.py
@@ -0,0 +1,129 @@
+import module_my.official_competition_bar_graph as ocbg
+import os
+
+rank_throughPass = 18
+rank_lobbedThroughPassTry = 1
+
+rank_possession = 50
+rank_pass = 114
+
+rank_longPassTry = 3
+rank_shootHeading = 1
+
+rank_goalHeading = 0.16
+
+rank_shootOutPenalty = 2
+
+#메인 실행 함수
+def main():
+    match_count = 30
+    nickname = input("닉네임을 입력하세요 : ")
+
+    #공격 플레이 스타일 분석
+    play_style = getPlayStyle(nickname, match_count);
+
+    #평균 경기 득점 수
+    avgoalCount = str(ocbg.avgoalTotal(match_count, nickname))
+
+    #주요 득점방식
+    goalMax = (getGoalCode(getMaxGoal(match_count, nickname)))
+
+    #결과 출력
+    print("[ FIFA ONLINE4 분석 도우미 ]")
+    print("- 닉네임 : %s" %nickname)
+    print("- 평균 경기(%d) 득점 수 : %s" %(match_count, avgoalCount))
+    print("- 주요 득점 방식 : %s" %goalMax)
+    print("- 플레이 스타일 : %s" %play_style)
+
+    #승률 그래프 100경기 기준
+    ocbg.winningRate(100, nickname)
+    os.system('pause')
+
+#스타일 코드
+def getPlayStyleCodeMessage(style_code):
+    dict = {
+        0 : "침투 플레이",
+        1 : "점유율 플레이",
+        2 : "타겟맨 플레이",
+        3 : "중거리 플레이",
+        4 : "밸런스 플레이"
+    }
+    return dict[style_code]
+
+#스타일 분석해서 코드 반환
+def getPlayStyle(nickname, match_count):
+    #(0=침투플레이) (1=점유율플레이) (2=타겟맨플레이) (3=중거리플레이) (4=벨런스플레이=>아무것도해당X)
+    plyer_throughPass = ocbg.avthroughPassTry(match_count, nickname)
+    plyer_lobbedThroughPassTry = ocbg.avlobbedThroughPassTry(match_count, nickname)
+
+    plyer_possession = ocbg.avpossession(match_count, nickname)
+    plyer_pass = ocbg.avpassTry(match_count, nickname)
+
+    plyer_longPassTry = ocbg.avlongPassTry(match_count, nickname)
+    plyer_shootHeading = ocbg.avshootHeading(match_count, nickname)
+
+    plyer_goalHeading = ocbg.avgoalHeading(match_count, nickname)
+
+    plyer_avshootOutPenalty = ocbg.avshootOutPenalty(match_count, nickname)
+
+    style_code = 4 
+
+    if(plyer_throughPass > rank_throughPass and plyer_lobbedThroughPassTry > rank_lobbedThroughPassTry):
+        style_code = 0
+    elif(plyer_possession > rank_possession and plyer_pass > rank_pass):
+        style_code = 1
+    elif(plyer_longPassTry > rank_longPassTry and plyer_goalHeading > rank_goalHeading and plyer_shootHeading > rank_shootHeading):
+        style_code = 2
+    elif(plyer_avshootOutPenalty > rank_shootOutPenalty):
+        style_code = 3
+    return getPlayStyleCodeMessage(style_code)
+
+
+#주요 득점 코드
+def getGoalCode(style_code):
+    dict = {
+        0 : "프리킥 골",
+        1 : "패널티 박스 안 골",
+        2 : "패널티킥 골",
+        3 : "헤딩 골",
+        4 : "패널티 박스 바깥 골",
+    }
+    return dict[style_code]
+
+#가장 많은 득점 패턴 반환
+def getMaxGoal(matchCount, nickname):
+    dic = {
+        "득점수" : 0,
+        "패턴" : "없음"
+    }
+    max = 0
+
+    goal_list = [
+        ocbg.avfreeKick(matchCount, nickname),
+        ocbg.avgoalInPenalty(matchCount, nickname),
+        ocbg.avgoalPenaltyKick(matchCount, nickname),
+        ocbg.avgoalHeading(matchCount, nickname),
+        ocbg.avgoalOutPenalty(matchCount, nickname)
+    ]
+
+    if(goal_list[0] > max):
+        max = goal_list[0]
+        dic["패턴"] = 0
+    if(goal_list[1] > max):
+        max = goal_list[1]
+        dic["패턴"] = 1
+    if(goal_list[2] > max):
+        max = goal_list[2]
+        dic["패턴"] = 2
+    if(goal_list[3] > max):
+        max = goal_list[3]
+        dic["패턴"] = 3
+    if(goal_list[4] > max):
+        max = goal_list[4]
+        dic["패턴"] = 4
+
+    return dic["패턴"]
+
+
+#실행
+main()
---
+++ ct01_5조최종결과/소스코드/official_competition_bar_graph.py
@@ -0,0 +1,217 @@
+# name : officialCompetition
+# author : 이정진
+# date : 2022-09-07
+
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
+#사용자 키 (넥슨에서 발급)
+api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjEzNDMwOTAzNzgiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY3ODAwOTAyNSwiaWF0IjoxNjYyNDU3MDI1LCJuYmYiOjE2NjI0NTcwMjUsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.tS9ocmoEg2BK-UzoGT60178omldYyIQhmsJ01fs_ErU"
+
+#헤더
+headers = {'Authorization' : api_key}
+
+
+#승률 그래프 함수
+def winningRate(matchCount, nickname):
+      
+    #승무패 변수와 그래프 관련 변수
+    x = np.arange(3)
+    result = ["승", "무", "패"] 
+    resultCount = [0, 0, 0] #승, 무, 패 순서
+
+    #매치 아이디 조회
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+
+        #매치 정보 조회
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["matchDetail"]["matchResult"]; #승무패 변수
+
+        if match_result == "승":
+            resultCount[0] = resultCount[0] + 1
+        elif match_result == "무":
+            resultCount[1] = resultCount[1] + 1
+        elif match_result == "패":
+            resultCount[2] = resultCount[2] + 1
+
+    result[0] = "승(" + str(resultCount[0]) + "%)"
+    result[1] = "무(" + str(resultCount[1]) + "%)"
+    result[2] = "패(" + str(resultCount[2]) + "%)"
+
+    plt.bar(x, resultCount, width=0.5)
+    plt.xticks(x, result)
+    plt.title("1 vs 1 공식 경기")
+    plt.show() 
+
+#매치 정보 조회
+def matchInfoFind(match_id):
+    match_info_url = 'https://api.nexon.co.kr/fifaonline4/v1.0/matches/'+match_id 
+    match_info_url_req = requests.get(match_info_url, headers = headers)
+    match_info_data = match_info_url_req.json()
+    return match_info_data
+
+#매치 아이디 조회
+def matchIdFind(matchtype, offset, limit, nickname):
+    match_info = {'matchtype' : matchtype, 'offset' : offset, 'limit' : limit}
+    match_url = 'https://api.nexon.co.kr/fifaonline4/v1.0/users/'+ userIdFind(nickname) + '/matches?'
+    match_url_req = requests.get(match_url,params=match_info, headers = headers)
+    match_url_data = match_url_req.json()
+    #매치 정보, 매치 데이터(json) 반환
+    return match_info, match_url_data
+
+#사용자 id 조회
+def userIdFind(nickname):
+    user_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users?"
+    player_nick = {'nickname' : nickname}
+    user_response = requests.get(user_url, params=player_nick, headers = headers)
+    user_json = user_response.json()
+    user_id = user_json['accessId']
+    return user_id
+
+#평균 쓰루 패스 시도 수
+def avthroughPassTry(matchCount, nickname):
+    av_result = 0 
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["pass"]["throughPassTry"]; 
+        av_result = av_result + int(match_result)
+    return int(av_result/matchCount);
+    
+#평균 로빙 쓰루 패스 시도 수
+def avlobbedThroughPassTry(matchCount, nickname):
+    av_result = 0 
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["pass"]["lobbedThroughPassTry"]; 
+        av_result = av_result + int(match_result)
+    return int(av_result/matchCount);
+
+#평균 점유율 (%단위로 반환)
+def avpossession(matchCount, nickname):
+    av_possession = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["matchDetail"]["possession"]; 
+        av_possession = av_possession + int(match_result)
+    return int(av_possession/matchCount)
+
+#평균 패스 시도 수
+def avpassTry(matchCount, nickname):
+    av_passTry = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["pass"]["passTry"]; 
+        av_passTry = av_passTry + int(match_result)
+    return int(av_passTry/matchCount)
+
+
+#평균 롱패스 시도 수
+def avlongPassTry(matchCount, nickname):
+    av_longPassTry = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["pass"]["longPassTry"]; 
+        av_longPassTry = av_longPassTry + int(match_result)
+    return int(av_longPassTry/matchCount)
+
+#평균 경기 전체 골 수
+def avgoalTotal(matchCount, nickname):
+    av_goalTotal = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalTotal"]; 
+        av_goalTotal = av_goalTotal + int(match_result)
+    return int(av_goalTotal/matchCount)
+
+#평균 헤딩 슛 수
+def avshootHeading(matchCount, nickname):
+    av_shootHeading = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["shootHeading"]; 
+        av_shootHeading = av_shootHeading + int(match_result)
+    return int(av_shootHeading/matchCount)
+
+
+#평균 인패널티 슛 수
+def avshootInPenalty(matchCount, nickname):
+    av_shootInPenalty = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["shootInPenalty"]; 
+        av_shootInPenalty = av_shootInPenalty + int(match_result)
+    return int(av_shootInPenalty/matchCount)
+
+#평균 아웃패널티 슛 수
+def avshootOutPenalty(matchCount, nickname):
+    av_shootOutPenalty = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["shootOutPenalty"]; 
+        av_shootOutPenalty = av_shootOutPenalty + int(match_result)
+    return int(av_shootOutPenalty/matchCount)
+
+#평균 헤딩 골 수
+def avgoalHeading(matchCount, nickname):
+    av_goalHeading = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): #매치를 하나씩 검사해서 승무패 카운트 올리기
+        match_id = match_url_data[i]; #리스트로 특정 매치 불러오기
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalHeading"]; 
+        av_goalHeading = av_goalHeading + int(match_result)
+    return float(av_goalHeading/matchCount)
+
+# 평균 인 패널티 골 수
+def avgoalInPenalty(matchCount, nickname):
+    goalInPenalty = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): 
+        match_id = match_url_data[i]
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalInPenalty"]
+        goalInPenalty = goalInPenalty + int(match_result)
+    return int(goalInPenalty/matchCount)
+
+# 아웃 패널티 골 수
+def avgoalOutPenalty(matchCount, nickname):
+    goalOutPenalty = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): 
+        match_id = match_url_data[i]
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalOutPenalty"]
+        goalOutPenalty = goalOutPenalty + int(match_result)
+    return int(goalOutPenalty/matchCount)
+
+# 프리킥 골 수
+def avfreeKick(matchCount, nickname):
+    FreeKick = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): 
+        match_id = match_url_data[i]
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalFreekick"]
+        FreeKick = FreeKick + int(match_result)
+    return int(FreeKick/matchCount)
+
+# 패널티킥 골 수
+def avgoalPenaltyKick(matchCount, nickname):
+    goalPenaltyKick = 0
+    match_info, match_url_data = matchIdFind(40, 0, matchCount, nickname)
+    for i in range(0, match_info['limit'], 1): 
+        match_id = match_url_data[i]
+        match_result = matchInfoFind(match_id)["matchInfo"][0]["shoot"]["goalPenaltyKick"]
+        goalPenaltyKick = goalPenaltyKick + int(match_result)
+    return int(goalPenaltyKick/matchCount)
+
+
+
